9b123298228bbfbbcea3e124093566a85060d60a
jonomy <ceciliabelmar12345@gmail.com>
2017-10-25 20:27:40 -0300

    tarea3

---
+++ src/proyecto1cecilia/Test1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1cecilia;
+
+/**
+ *
+ * @author LAB06
+ */
+public class Test1 
+{
+    
+    public static void main(String[] args)
+   {
+        int numero;//Declaracion de la variable tipo entero
+        double numero2;//Declaracion variable tipo double
+        String name;//Aqui guardaremos texto
+        float decimal;//Declaracion variable tipo decimal
+        boolean respuesta =true;//Declaracio e inicializacion
+        
+        numero=60;// Acabo de inicializar la variable numero
+        numero2=3.1564564654;//Inicializo la variable numero2 
+        name="Cecilia Belmar";//Asigno un valor variable name
+        decimal=3.15f;//Agrego literal f para evitar perdida de presicion
+        //En este bloque vamos a usar las variables
+        System.out.println("Suma de numero y numero2:"+(numero+numero2));
+        System.out.println("Bienvenido\r"+name);
+           
+        
+        
+    }
+}
